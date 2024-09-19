51b4ef386b57f17a408500e470b395b9649edcf3
DinAstral <princemamaril35@gmail.com>
2024-09-19 21:02:33 +0800

    app remove '/'

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ import UserTrackOrder from './User/UserTrackOrder';
 
 import ProtectedRoutes from './hooks/ProtectedRoutes';
 
-axios.defaults.baseURL = 'https://nuifms-predep-10ceea2df468.herokuapp.com/';
+axios.defaults.baseURL = 'https://nuifms-predep-10ceea2df468.herokuapp.com';
 axios.defaults.withCredentials = true;
 
 function App() {
