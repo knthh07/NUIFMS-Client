f0a73fd5cf53310dc6df4cd5d0e70dc318ca51d6
DinAstral <princemamaril35@gmail.com>
2024-09-23 11:02:24 +0800

    signup

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -12,11 +12,10 @@ const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [emailError, setEmailError] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false); // For terms modal
-  const [hasScrolledToEnd, setHasScrolledToEnd] = useState(false); // For scroll detection
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [data, setData] = useState({ email: '', password: '', confirmPassword: '' });
-  const termsRef = useRef(null); // Ref for the modal content
   const navigate = useNavigate();
+  const termsRef = useRef(null);
 
   const toggleShowPassword = () => setShowPassword(!showPassword);
   const toggleShowConfirmPassword = () => setShowConfirmPassword(!showConfirmPassword);
@@ -29,12 +28,12 @@ const Signup = () => {
   };
 
   const openModal = (e) => {
-    e.preventDefault(); // Prevent form from being submitted immediately
+    e.preventDefault();
     const { password, confirmPassword } = data;
     if (password !== confirmPassword) {
       toast.error('Passwords do not match');
     } else {
-      setIsModalOpen(true); // Open the terms modal
+      setIsModalOpen(true);
     }
   };
 
@@ -62,7 +61,7 @@ const Signup = () => {
 
   const handleAccept = () => {
     setIsModalOpen(false);
-    registerUser(); // Trigger user registration
+    registerUser();
   };
 
   const handleRefuse = () => {
@@ -86,90 +85,114 @@ const Signup = () => {
                 variant='filled'
                 label='Email'
                 fullWidth
+                InputLabelProps={{
+                  style: { color: 'white' },
+                }}
+                sx={{
+                  input: { color: 'white' },
+                  '& .MuiFilledInput-root': {
+                    backgroundColor: 'transparent',
+                    borderBottom: '1px solid white',
+                  },
+                  '& .Mui-focused .MuiFilledInput-input': {
+                    backgroundColor: 'transparent',
+                  },
+                  '& .Mui-focused': {
+                    borderColor: 'white',
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white',
+                  }
+                }}
                 value={data.email}
                 required
                 onChange={handleEmailChange}
                 error={!!emailError}
                 helperText={emailError}
-                sx={{
-                  input: {
-                    color: 'white',
-                    backgroundColor: '#1b1b38',
-                    borderRadius: '5px'
-                  },
-                  '& .MuiFilledInput-root': {
-                    backgroundColor: '#1b1b38',
-                    borderBottom: '1px solid white',
-                  },
-                  '& .MuiInputLabel-root': {
-                    color: 'white'
-                  },
-                  '& .MuiFormHelperText-root': {
-                    color: '#FF9494'
-                  }
-                }}
               />
               <TextField
                 variant='filled'
                 type={showPassword ? 'text' : 'password'}
                 label='Password'
+                InputLabelProps={{
+                  style: { color: 'white' },
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={toggleShowPassword}
+                        edge="end"
+                        style={{ color: "white" }}
+                      >
+                        {showPassword ? <VisibilityOff style={{ color: 'white' }} /> : <Visibility style={{ color: 'white' }} />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                fullWidth
+                sx={{
+                  input: { color: 'white' },
+                  '& .MuiFilledInput-root': {
+                    backgroundColor: 'transparent',
+                    borderBottom: '1px solid white',
+                  },
+                  '& .Mui-focused .MuiFilledInput-input': {
+                    backgroundColor: 'transparent',
+                  },
+                  '& .Mui-focused': {
+                    borderColor: 'white',
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white',
+                  }
+                }}
                 value={data.password}
                 required
                 onChange={(e) => setData({ ...data, password: DOMPurify.sanitize(e.target.value) })}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={toggleShowPassword} edge="end" style={{ color: "white" }}>
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                sx={{
-                  input: {
-                    color: 'white',
-                    backgroundColor: '#1b1b38',
-                    borderRadius: '5px'
-                  },
-                  '& .MuiFilledInput-root': {
-                    backgroundColor: '#1b1b38',
-                    borderBottom: '1px solid white',
-                  },
-                  '& .MuiInputLabel-root': {
-                    color: 'white'
-                  }
-                }}
               />
               <TextField
                 variant='filled'
-                type={showConfirmPassword ? 'text' : 'password'}
                 label='Confirm Password'
-                value={data.confirmPassword}
-                required
-                onChange={(e) => setData({ ...data, confirmPassword: DOMPurify.sanitize(e.target.value) })}
+                type={showConfirmPassword ? 'text' : 'password'}
+                fullWidth
+                InputLabelProps={{
+                  style: { color: 'white' },
+                }}
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
-                      <IconButton onClick={toggleShowConfirmPassword} edge="end" style={{ color: "white" }}>
-                        {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={toggleShowConfirmPassword}
+                        edge="end"
+                        style={{ color: "white" }}
+                      >
+                        {showConfirmPassword ? <VisibilityOff style={{ color: 'white' }} /> : <Visibility style={{ color: 'white' }} />}
                       </IconButton>
                     </InputAdornment>
                   ),
                 }}
                 sx={{
-                  input: {
-                    color: 'white',
-                    backgroundColor: '#1b1b38',
-                    borderRadius: '5px'
-                  },
+                  input: { color: 'white' },
                   '& .MuiFilledInput-root': {
-                    backgroundColor: '#1b1b38',
+                    backgroundColor: 'transparent',
                     borderBottom: '1px solid white',
                   },
-                  '& .MuiInputLabel-root': {
-                    color: 'white'
+                  '& .Mui-focused .MuiFilledInput-input': {
+                    backgroundColor: 'transparent',
+                  },
+                  '& .Mui-focused': {
+                    borderColor: 'white',
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white',
                   }
                 }}
+                value={data.confirmPassword}
+                required
+                onChange={(e) => setData({ ...data, confirmPassword: DOMPurify.sanitize(e.target.value) })}
               />
             </div>
             <button type='submit' className="bg-[#5cb85c] text-white border-none rounded-md cursor-pointer block py-2 px-8 mx-auto hover:bg-[#449D44]">Sign Up</button>
@@ -192,16 +215,14 @@ const Signup = () => {
             overflowY: 'auto',
           }}>
             <h2>Terms and Conditions</h2>
-            <p>[
-              'Welcome to NUIFMS! This privacy policy explains how we collect, use, disclose, and protect your personal information.
+            <p>[Welcome to NUIFMS! This privacy policy explains how we collect, use, disclose, and protect your personal information.
               By using our app/website, you agree to the practices described in this policy.
               nPersonal Information: This may include your name, email address, phone number, and any other information you provide directly.
               nUsage Data: Information about how you use our app/website, including your IP address, browser type, and pages visited.
               nWe implement reasonable security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction.
               However, no data transmission over the internet or electronic storage system is 100% secure.
               We may update this privacy policy from time to time. We will notify you of any changes by posting the new policy on our app/website.
-              Your continued use of our services after such changes indicates your acceptance of the updated policy
-              ]</p>
+              Your continued use of our services after such changes indicates your acceptance of the updated policy]</p>
             <Button onClick={handleAccept} style={{ backgroundColor: '#5cb85c', color: 'white' }}>Accept</Button>
             <Button onClick={handleRefuse} style={{ backgroundColor: '#d9534f', color: 'white', marginLeft: '10px' }}>Refuse</Button>
           </div>
@@ -212,5 +233,3 @@ const Signup = () => {
 };
 
 export default Signup;
-
-
