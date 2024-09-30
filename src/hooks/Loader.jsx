a2816ead90a8a129c1dd7447c8e2ebc4ea0b2038
DinAstral <princemamaril35@gmail.com>
2024-09-30 11:39:03 +0800

    styling the loader

--- src/hooks/Loader.jsx
+++ src/hooks/Loader.jsx
@@ -3,17 +3,21 @@ import React from 'react';
 import { ClipLoader } from 'react-spinners';
 
 const Loader = ({ isLoading }) => {
-  const loaderStyle = {
+  const loaderContainerStyle = {
     position: 'fixed',
     top: '10px',
     right: '10px',
-    zIndex: '9999', // Make sure it appears on top of other content
+    zIndex: '9999', // Ensure loader is always on top
+    padding: '10px',
+    borderRadius: '10px',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)', // Semi-transparent background
+    boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)', // Soft shadow for depth
   };
 
   return (
     isLoading && (
-      <div style={loaderStyle}>
-        <ClipLoader color="#000" loading={isLoading} size={50} />
+      <div style={loaderContainerStyle}>
+        <ClipLoader color="#35408e" loading={isLoading} size={45} />
       </div>
     )
   );
